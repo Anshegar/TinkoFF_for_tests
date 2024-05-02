7bb37ffe0832fcba85f506c89aec8975d1098726
Anshegar <mtxvoron@mail.ru>
2024-05-02 15:37:16 +0300

    first

--- app.py
+++ app.py
@@ -42,10 +42,9 @@
 import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask import Flask, render_template,url_for,redirect
+from flask import Flask, render_template, redirect, url_for, request, jsonify
 
-# Формы
-from flask import render_template, redirect, url_for, request
+
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -76,14 +75,14 @@ class Answer(db.Model):
 
 
 
+@app.route('/')
+def index():
+    themes = Theme.query.order_by(Theme.name).all()
+    themes_with_index = [(index, theme) for index, theme in enumerate(themes)]
+    return render_template('base.html', themes_with_index=themes_with_index)
+
 
 # 2. Форма для добавления новой темы
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    return render_template('base.html')
-
-
 @app.route('/add_theme', methods=['GET', 'POST'])
 def add_theme():
     if request.method == 'POST':
@@ -99,28 +98,179 @@ def add_theme():
 
 
 
+# 3. Удаление 
+@app.route('/delete_theme', methods=['GET', 'POST'])
+def delete_theme():
+    return render_template('delete_theme.html')
+
+# - Удаление темы по ID
+@app.route('/delete_theme_by_id', methods=['POST'])
+def delete_theme_by_id():
+    theme_id = request.form['theme_id']
+    theme = Theme.query.get(theme_id)
+    if theme:
+        db.session.delete(theme)
+        db.session.commit()
+        return "Тема успешно удалена"
+    else:
+        return "Такой темы не существует"
+
+# - Удаление темы по Имени
+@app.route('/delete_theme_by_name', methods=['POST'])
+def delete_theme_by_name():
+    theme_name = request.form['theme_name']
+    theme = Theme.query.filter_by(name=theme_name).first()
+    if theme:
+        db.session.delete(theme)
+        db.session.commit()
+        return "Тема успешно удалена"
+    else:
+        return "Такой темы не существует"
+
+
+
+@app.route('/update_theme', methods=['GET', 'POST'])
+def update_theme():
+    return render_template('update_theme.html')
+
+
+# 5. Модификация темы по id
+@app.route('/update_theme_by_id', methods=['POST'])
+def update_theme_by_id():
+    theme_id = request.form['theme_id']
+    new_theme_name = request.form['new_theme_name']
+    theme = Theme.query.get(theme_id)
+    if theme:
+        theme.name = new_theme_name
+        db.session.commit()
+        return "Тема успешно модифицирована"
+    else:
+        return "Такой темы не существует"
+
+# 6. Модификация темы по имени
+@app.route('/update_theme_by_name', methods=['POST'])
+def update_theme_by_name():
+    theme_name = request.form['theme_name']
+    new_theme_name = request.form['new_theme_name']
+    theme = Theme.query.filter_by(name=theme_name).first()
+    if theme:
+        theme.name = new_theme_name
+        db.session.commit()
+        return "Тема успешно модифицирована"
+    else:
+        return "Такой темы не существует"
+
+
+
+# --------------------------------------------------------------!!!!
+
 
 # 3. Форма для добавления новых ответов
 @app.route('/add_answer', methods=['GET', 'POST'])
 def add_answer():
     if request.method == 'POST':
-        tags = request.form['tags']
-        answer = request.form['answer']
-        theme_id = request.form['theme_id']
-        
-        theme = Theme.query.get(theme_id)
-        new_answer = Answer(tags=tags, answer=answer, theme=theme)
+        theme_name = request.form['theme_name']
+        tags = request.form['tags'].split(',')
+        answer_text = request.form['answer_text']
+
+        # Найдите или создайте тему
+        theme = Theme.query.filter_by(name=theme_name).first()
+        if not theme:
+            theme = Theme(name=theme_name)
+            db.session.add(theme)
+            db.session.commit()
+
+        # Создайте новый ответ
+        new_answer = Answer(tags=','.join(tags), answer=answer_text, theme=theme)
         db.session.add(new_answer)
         db.session.commit()
+
         return redirect(url_for('add_answer'))
-    
+
     themes = Theme.query.all()
     return render_template('add_answer.html', themes=themes)
 
 
 
+@app.route('/delete_answer', methods=['GET', 'POST'])
+def delete_answer():
+    return render_template('delete_answer.html')
 
-# 4. Форма для поиска ответов
+# Удаление ответа по ID
+@app.route('/delete_answer_by_id', methods=['POST'])
+def delete_answer_by_id():
+    answer_id = request.form['answer_id']
+    answer = Answer.query.get(answer_id)
+    if answer:
+        db.session.delete(answer)
+        db.session.commit()
+        return "Ответ успешно удален"
+    else:
+        return "Такого ответа не существует"
+
+
+
+#-----------------------------------------------------------!!!!
+@app.route('/update_answer', methods=['GET', 'POST'])
+def update_answer():
+    if request.method == 'POST':
+        answer_id = request.form['answer_id']
+        theme_name = request.form['theme_name']
+        tags_to_add = [tag.strip() for tag in request.form['tags_to_add'].split(',') if tag.strip()]
+        tags_to_remove = [tag.strip() for tag in request.form['tags_to_remove'].split(',') if tag.strip()]
+        answer_text = request.form['answer_text']
+
+        # Найдите ответ по ID
+        answer = Answer.query.get(answer_id)
+        if answer:
+            # Обновление данных ответа
+            if theme_name:
+                theme = Theme.query.filter_by(name=theme_name).first()
+                if theme:
+                    answer.theme = theme
+            if tags_to_add:
+                existing_tags = set(answer.tags.split(',')) if answer.tags else set()
+                new_tags = existing_tags.union(tags_to_add)
+                answer.tags = ','.join(new_tags)
+            if tags_to_remove:
+                existing_tags = answer.tags.split(',')
+                new_tags = [tag for tag in existing_tags if tag not in tags_to_remove]
+                answer.tags = ','.join(new_tags)
+            if answer_text:
+                answer.answer = answer_text
+
+            db.session.commit()
+            
+            print("Ответ успешно обновлен")
+            return redirect(url_for("update_answer"))
+        else:
+            print("Такого ответа не существует")
+            return redirect(url_for("update_answer"))
+
+    # Получите список всех тем для заполнения формы
+    themes = Theme.query.all()
+    return render_template('update_answer.html', themes=themes)
+
+
+
+
+
+#-------------------------------------------------------------!!!!
+
+# --- Форма для изменения Темы
+# --- Форма в HTML будеn содержать уже заполненные поля взятые из БД, все кроме ID и 2 кнопки. 
+# --- --- Изменить, отправит форму обратно в БД , надо будет сделать подтверждение
+# --- --- Удалить, надо будет сделать подтверждение
+# --- Форма для изменения ответов
+# --- Форма в HTML будеn содержать уже заполненные поля взятые из БД, все кроме ID и 2 кнопки. 
+# --- --- Изменить, отправит форму обратно в БД , надо будет сделать подтверждение
+# --- --- Удалить, надо будет сделать подтверждение
+ 
+
+
+
+
+# Форма для поиска ответов
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     if request.method == 'POST':
